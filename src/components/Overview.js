6ea1e1eb6b13007e2933d750efdfb65900ff0faa
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-28 11:31:51 -0600

    fixed all of the bugs and got rid of all of the warning messages

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -5,40 +5,35 @@ const Overview = ({ tasks, delTask, submitEdits, todoEditing, settodoEditing, se
 
   return (
     <div>
-      { tasks.length === 0 &&
-        <li>No more tasks to complete</li>
-      }
-      {tasks.map((task) => {
-        return (
-          <div>
-            <li key={task.id}>
-              {tasks.indexOf(task) + 1 }.{' '}
+      <ul>
+        { tasks.length === 0 &&
+          <li>No more tasks to complete</li>
+        }
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {tasks.indexOf(task) + 1 }.{' '}
+            {task.id === todoEditing ? (
+              <input
+                type='text'
+                onChange={(e) => setEditingText(e.target.value)}
+              />
+            ) : (
+              <div>{task.text}</div>
+            )}  
+            <div className="btnBox">
               {task.id === todoEditing ? (
-                <input
-                  type='text'
-                  onChange={(e) => setEditingText(e.target.value)}
-                />
+                <button onClick={() => submitEdits(task.id)}>Resubmit</button>
               ) : (
-                <div>{task.text}</div>
-              )}  
-              <div className="btnBox">
-                {task.id === todoEditing ? (
-                  <button onClick={() => submitEdits(task.id)}>Resubmit</button>
-                ) : (
-                  <button onClick={() => settodoEditing(task.id)}>Edit</button>
-                )}
-                {' '}
-                <button 
-                  onClick={() => delTask(task.id)}>
-                    Delete
-                </button>
-              </div>
-            </li>
-          </div>
-        )
-      })}
-      <ul className="container">
-        
+                <button onClick={() => settodoEditing(task.id)}>Edit</button>
+              )}
+              {' '}
+              <button 
+                onClick={() => delTask(task.id)}>
+                  Delete
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
